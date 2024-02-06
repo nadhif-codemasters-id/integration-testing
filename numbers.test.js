1daecf51366253dd32cf0858863e5d7d2c9c00ff
nadhif-codemasters-id <nadhif@codemasters.id>
2024-02-06 13:31:01 +0700

    initial commit

---
+++ numbers.test.js
@@ -0,0 +1,26 @@
+import { cleanNumbers } from "./numbers";
+import { it, expect } from "vitest";
+import { validateStringNotEmpty } from "./validation";
+
+it("should clean numbers", () => {
+  // Arrange
+  const numbers = ["1", "2", "3"];
+  const expected = [1, 2, 3];
+
+  // Act
+  const result = cleanNumbers(numbers);
+
+  // Assert
+  expect(result).toEqual(expected);
+});
+
+it("should throw an error if the input is not a valid number", () => {
+  // Arrange
+  const numbers = ["1", "2", "a"];
+
+  // Act
+  const result = () => cleanNumbers(numbers);
+
+  // Assert
+  expect(result).toThrowError("Invalid number input");
+});
